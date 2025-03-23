1d2d5970bd80b0c342c37ef2be93218455530910
gracechristine <graciechristineli@gmail.com>
2025-03-22 22:11:04 -0600

    Completed training - multiply.

---
+++ 8 kyu/Multiply.cs
@@ -0,0 +1,10 @@
+// 6 kyu - Multiples of 3 or 5
+
+// This code does not execute properly. Try to figure out why.
+
+public class CustomMath {
+    public static int multiply(int a, int b) {
+//         return a b;
+          return a * b;
+    }
+}
---
+++ 8 kyu/Multiply.py
@@ -0,0 +1,7 @@
+# 6 kyu - Multiples of 3 or 5
+
+# This code does not execute properly. Try to figure out why.
+
+def multiply(a, b):
+#     a * b
+    return a * b
