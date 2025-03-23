70dced65d969459b2c683ee6ee14b6da2ebe0005
gracechristine <graciechristineli@gmail.com>
2025-03-23 10:33:42 -0600

    Finished training again - multiply

---
+++ 8 kyu/Multiply.ts
@@ -0,0 +1,7 @@
+// 8 kyu - Return Negative
+
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+export function multiply(a: number, b: number): number {
+    return a * b;
+  }
