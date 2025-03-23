1bd803998f33aed26cbee583010c8546c57db6c2
gracechristine <graciechristineli@gmail.com>
2025-03-23 10:23:43 -0600

    Finished additioanl training on the same one - even or odd

---
+++ 8 kyu/Even or Odd.coffee
@@ -0,0 +1,6 @@
+# 8 kyu - Return Negative
+
+# In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+evenOrOdd = (number) ->
+   if number % 2 == 0 then "Even" else "Odd"
---
+++ 8 kyu/Even or Odd.ts
@@ -0,0 +1,7 @@
+// 8 kyu - Return Negative
+
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+export function evenOrOdd(n:number):string {
+    return n % 2 == 0 ? "Even" : "Odd";
+  }
