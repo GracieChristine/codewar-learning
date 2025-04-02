0da411eabbd5bc54c066d1a8815cd703790b2858
gracechristine <graciechristineli@gmail.com>
2025-04-02 15:48:05 -0600

    Finished session - opposite number

---
+++ 8 kyu/Opposite Number.js
@@ -0,0 +1,12 @@
+// 8 kyu - Opposite Number
+
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+function opposite(number) {
+    return -number;
+}
+
+function opposite(number) {
+    return number * -1;
+}
+
---
+++ 8 kyu/Opposite Number.ts
@@ -0,0 +1,15 @@
+// 8 kyu - Opposite Number
+
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+export class Kata1 {
+    static opposite(n: number) {
+      return -n;
+    }
+}
+
+export class Kata {
+    static opposite(n: number) {
+      return n * -1;
+    }
+}
